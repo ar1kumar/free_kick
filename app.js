465e9d2d8b33728756a1417dbe9bea407fe205be
Arun Kumaran <arunkmr492@gmail.com>
2017-02-22 16:24:18 +0530

    Finally its here, alpha build

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-var express =require('express');
-  var express = require('express'),
-    app = express()
-  , http = require('http')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
+/*
+Authors - Arun Kumaran, Samir Merchant
+Github - @ar1kumar, @fangedparakeet
+Cheers bros.
+*/
+
+// var express =require('express');
+//   var express = require('express'),
+//     app = express()
+//   , http = require('http')
+//   , server = http.createServer(app)
+//   // EventEmitter = require('events')
+//   , io = require('socket.io').listen(server);
+
+  var express =require('express');
+  var app = express();
+    // , http = require('http')
+    // , server = http.createServer(app)
+    // , io = require('socket.io').listen(server);
+
+  // listen for new web clients:
+  // server.listen(5000);
+  var io = require('socket.io').listen(app.listen(process.env.PORT || 5000));
+
+ var shot, save;
 
 // listen for new web clients:
-server.listen(8080);
+//server.listen(5000);
 
      //------------------- Default Shit--------------------------------------//
 app.configure(function(){
@@ -30,28 +49,33 @@ app.configure('production', function(){
 
 //------------------- Default Shit ends--------------------------------------//
 
-var request = require('request');
-var spark = require('sparkcloud')('eabf911dc1619fcba51d56d543b8bba6e318b4f8')
-var core = spark.device('53ff6e065075535142251687')
- var up = false;
-
 
 //Socket functions starts//
 io.sockets.on('connection', function (socket) {
-	socket.on('add', function(data) {
-		io.sockets.emit('fire', data);
-	});
-	
-	socket.on('begin', function(){
-		io.sockets.emit('started');
-	});
-	
-	socket.on('remove', function() {
-		io.sockets.emit('remove_particle');
-	});
-	
-	socket.on('add_name', function(data){
-		io.sockets.emit('add_name1', data);
+	console.log('yolo');
+
+
+	socket.on('kick', function(data) {
+		shot = data.dir;
+		if(save != null){
+			penalty(shot, save);
+		}
 		console.log(data);
 	});
+
+	socket.on('block', function(data) {
+		console.log(data);
+		save = data.block_dir;
+		if(shot != null){
+			penalty(shot, save);
+		}
+	});
+
+
 });
+
+function penalty(theshot, thesave){
+	io.sockets.emit('kicked', {'shot': theshot, 'save': thesave});
+	shot = null;
+	save = null;
+}
